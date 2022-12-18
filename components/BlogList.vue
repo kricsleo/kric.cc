2fa4e1c173b326e7f33bb99fe733c86917eb6236
许圣峰 <kricsleo@163.com>
2022-12-18 14:08:14 +0800

    feat: blog list

---
+++ components/BlogList.vue
@@ -0,0 +1,9 @@
+<template>
+  <ContentList v-slot="content">
+    <ul class="prose">
+      <li v-for="article in content.list" :key="article._path" text-20 py-10>
+        <NuxtLink :to="article._path"> {{article.title}} </NuxtLink>
+      </li>
+    </ul>
+  </ContentList>
+</template>
