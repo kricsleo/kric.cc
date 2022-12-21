8cea557554a705c75c386ac5af3e374409133ae8
许圣峰 <kricsleo@163.com>
2022-12-21 11:33:21 +0800

    feat: add link target

---
+++ components/content/ProseA.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const props2 = defineProps({
+  href: {
+    type: String,
+    default: ''
+  },
+  blank: {
+    type: Boolean,
+    default: false
+  }
+})
+
+const target = computed(() => {
+  const isBlank = props2.blank || /^https?:/.test(props2.href)
+  return isBlank ? '_blank' : undefined
+})
+</script>
+
+<template>
+  <NuxtLink :href="href" :target="target">
+    <slot />
+  </NuxtLink>
+</template>
