5587832851a23642e57cdc285550bf7ecce896ce
许圣峰 <kricsleo@163.com>
2023-11-13 12:07:35 +0800

    fix: lighthouse problems

--- astro.config.ts
+++ astro.config.ts
@@ -1,16 +1,22 @@
 import { defineConfig } from 'astro/config';
 import UnoCSS from 'unocss/astro';
 import vue from "@astrojs/vue";
+import robotsTxt from "astro-robots-txt";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://kric.cc',
   prefetch: {
     defaultStrategy: 'viewport'
   },
   integrations: [
-    vue(),
+    vue(), 
     UnoCSS({
       injectReset: true
-    }),
-  ]
+    }), 
+  robotsTxt(), 
+  sitemap(),
+]
 });
