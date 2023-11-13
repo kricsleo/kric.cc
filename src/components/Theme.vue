cc1b36845e35adc22b2a18df7abbfe6938922045
许圣峰 <kricsleo@163.com>
2023-11-13 11:47:11 +0800

    feat: instantly dark mode

--- src/components/Theme.vue
+++
@@ -1,9 +0,0 @@
-<script setup lang="ts">
-import { useDark, useToggle } from '@vueuse/core'
-
-const toggleDark = useToggle(useDark())
-</script>
-
-<template>
-  <button class="i-carbon:sun dark:i-carbon:moon" @click="toggleDark()" />
-</template>
